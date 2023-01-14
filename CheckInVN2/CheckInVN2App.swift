77573ccee6c42e9841be4d3c7da14608189e1ffa
phong1412-1412 <phongbuibsp1@gmail.com>
2023-01-14 07:31:25 -0800

    Initial Commit

---
+++ CheckInVN2/CheckInVN2App.swift
@@ -0,0 +1,17 @@
+//
+//  CheckInVN2App.swift
+//  CheckInVN2
+//
+//  Created by PPPP on 14/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CheckInVN2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
