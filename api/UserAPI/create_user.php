4d07499fdc31ad1f1dd20c498dadc168d1669ab2
Phong1412-1412 <phongbuibsp1@gmail.com>
2023-02-17 07:41:13 +0700

    add

--- api/UserAPI/create_user.php
+++ api/UserAPI/create_user.php
@@ -1,72 +1,68 @@
 <?php
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
-    header("Content-Type: text/json; charset=UTF-8");
-    header('Content-Type: text/html; charset=UTF-8');
     header("Access-Control-Allow-Methods: POST");
     header("Access-Control-Max-Age: 3600");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     include_once '../../config/database.php';
-    include_once '../../class/nguoidung.php';
+    include_once '../../class/user.php';
+    
     $database = new Database();
     $db = $database->getConnection();
-    
-    if(isset($_POST['name'])) {
-         //Lấy dữ liệu từ file signup_user.dart
-        $username = $_POST['name'];
-        $email = $_POST['email'];
-        $password = $_POST['pass'];
-        $point = $_POST['point'];
-        $rank = $_POST['rank'];
+    mysqli_set_charset($db, "utf8");
+    // Kiểm tra nếu dữ liệu được gửi lên là POST
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        var_dump($_POST);
+        $username = $_POST['name'] ?? null;
+        $email = $_POST['email'] ?? null;
+        $password = $_POST['pass'] ?? null;
 
 
          //Kiểm tra người dùng đã nhập liệu đầy đủ chưa
-        if ($username == "" || $password == "" || $email == "" || $rank == "" || $point == "")
-        {
-            echo json_encode("Vui lòng nhập đầy đủ thông tin: '$username': '$email': '$password': '$point' : '$rank'");
+        if(empty($username)) {
+            echo json_encode("Vui lòng nhập tên đăng nhập");
             exit;
         }
-        // Mã khóa mật khẩu
-        // $password = md5($password);
+        if(empty($password)) {
+            echo json_encode("Vui lòng nhập mật khẩu");
+            exit;
+        }
+        if(empty($email)) {
+            echo json_encode("Vui lòng nhập địa chỉ email");
+            exit;
+        }
+        
         //Kiểm tra tên đăng nhập này đã có người dùng chưa
-        $sqlQuery = "SELECT * FROM nguoidung WHERE NguoiDung_Email = '$email'";
-        $res = mysqli_query($db,$sqlQuery);
-        $count =mysqli_num_rows($res);
-        if($count >= 1){
-            echo json_encode("account already exists");
+        $sqlQuery = "SELECT * FROM user WHERE user_Email = '$email'";
+        $res = mysqli_query($db, $sqlQuery);
+        $count = mysqli_num_rows($res);
+        if ($count >= 1) {
+            echo json_encode("Tài khoản đã tồn tại", JSON_UNESCAPED_UNICODE);
             exit;
         }
 
+        //Mã hóa mật khẩu
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        
         //Lưu thông tin thành viên vào bảng
-        //INSERT INTO nguoidung VALUES(null,"test",1,"test",0,1)
         $sqlQuery = ("
-            INSERT INTO nguoidung 
+            INSERT INTO user 
             VALUES (
                 null,
                 '$username',
                 '$password',
-                '$email',
-                '$point',
-                '$rank'
+                '$email'
             )
         ");
-
-        $addmemberSuccess = mysqli_query($db,$sqlQuery);
+        $addmemberSuccess = mysqli_query($db, $sqlQuery);
 
         //Thông báo quá trình lưu
-        if($addmemberSuccess)
-        {
-             echo json_encode("success");
+        if($addmemberSuccess) {
+            echo json_encode("success");
+        } else {
+            echo json_encode("Có lỗi xảy ra khi lưu thông tin tài khoản", JSON_UNESCAPED_UNICODE);
         }
-        else
-        {
-            echo json_encode("error");
-        }
-
+    } else {
+        echo json_encode("Phương thức gửi dữ liệu không hợp lệ. Vui lòng sử dụng phương thức POST", JSON_UNESCAPED_UNICODE);
     }
-    else
-    {
-        echo json_encode("chua nhan post");
-    }
-   
 ?>
--- api/UserAPI/login_user.php
+++ api/UserAPI/login_user.php
@@ -1,23 +1,28 @@
 <?php 
+	session_start();
 	require_once("../../config/database.php");
 	$database = new Database();
 	$db = $database->getConnection();
-	if(isset($_POST['email'])&&isset($_POST['pass']))
-	{
-		$email = $_POST['email'];
-    	$pass = $_POST['pass'];
 
-   		$sqlQuery = "SELECT * FROM nguoidung WHERE NguoiDung_Email = '".$email."' AND NguoiDung_PassWord='".$pass."'";
-    	$res = mysqli_query($db,$sqlQuery);
-    	$count =mysqli_num_rows($res);
-    	if($count >= 1){
-    		while($row = mysqli_fetch_assoc($res)) {
-    			echo json_encode($row['NguoiDung_ID']);
-    		}
-    	}
-    	else
-    	{
-    		echo json_encode("error");
-    	}
-    }
+	if(isset($_POST['email']) && isset($_POST['pass'])) {
+		$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+		$pass = filter_var($_POST['pass'], FILTER_SANITIZE_STRING);
+		$pass = hash('sha256', $pass);
+
+		$stmt = $db->prepare("SELECT * FROM user WHERE user_Email = ? AND user_PassWord = ?");
+        $stmt->bind_param("ss", $email, $pass);
+        $stmt->execute();
+        $res = $stmt->get_result();
+
+        if ($res->num_rows >= 1) {
+            $token = bin2hex(random_bytes(32));
+            $_SESSION['token'] = $token;
+            while ($row = mysqli_fetch_assoc($res)) {
+                $_SESSION['user_ID'] = $row['user_ID'];
+            }
+            echo json_encode(array('token' => $token));
+        } else {
+            echo json_encode("error");
+        }
+	}
 ?>
